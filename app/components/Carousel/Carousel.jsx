c8155eadc774ca234ac9baae08884cc60254b018
Luiz Gabriel Gomes <luizgabrielgomes9686@gmail.com>
2024-08-14 00:33:14 +0100

    update Carousel

--- app/components/Carousel/Carousel.jsx
+++ app/components/Carousel/Carousel.jsx
@@ -4,15 +4,15 @@ import banner from "../../../public/assets/banner.png"
 import banner2 from "../../../public/assets/banner2.png"
 
 export function CarouselComponent() { 
-  return (
-    <Carousel>
-      <Carousel.Item>
-        <Image src={banner} height={100} style={{ width: '100%', height: '350px' }} />
-      </Carousel.Item>
-      <Carousel.Item>
-      <Image src={banner2} height={100} style={{ width: '100%', height: '350px' }} />
-      </Carousel.Item>
-    </Carousel>
-  );
+    return (
+        <Carousel>
+            <Carousel.Item>
+                <Image src={banner} height={100} style={{ width: '100%', "object-fit": "cover"}} />
+            </Carousel.Item>
+            <Carousel.Item>
+                <Image src={banner2} height={100} style={{ width: '100%', "object-fit": "cover"}} />
+            </Carousel.Item>
+        </Carousel>
+    );
 }
 
