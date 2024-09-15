da9bd969abef7cf040f40af11bda48e04b5ceb70
Luiz Gabriel Gomes <luizgabrielgomes9686@gmail.com>
2024-09-15 23:11:45 +0100

    update questions

--- app/staticData/data.js
+++ app/staticData/data.js
@@ -17,22 +17,35 @@ export const data = [
             { alternative: "support or defend someone" , isCorrect: true},
         ]
     },
+
+
+
+
+    {
+        _id: "03",
+        title: "What is the mean of Get along/ Get on/ Get on with?",
+        alternatives: [
+            { alternative: "stay for a long time" , isCorrect: false },
+            { alternative: "take a long time" , isCorrect: false},
+            { alternative: "To have a good relationship" , isCorrect: true},
+        ]
+    },
+    {
+        _id: "04",
+        title: "What is the meaning of find out?",
+        alternatives: [
+            { alternative: "discover a fact or piece of information", isCorrect: true },
+            { alternative: "means to eventually decide something", isCorrect: false },
+            { alternative: "to have a good relationship with someone" , isCorrect: false},
+        ]
+    },
     // {
-    //     _id: "03",
-    //     title: "What is the mean of Get along/ Get on/ Get on with?",
+    //     _id: "05",
+    //     title: "What is the meaning of watch out?",
     //     alternatives: [
-    //         { alternative: "stay for a long time" , isCorrect: false },
-    //         { alternative: "take a long time" , isCorrect: false},
-    //         { alternative: "To have a good relationship" , isCorrect: true},
-    //     ]
-    // },
-    // {
-    //     _id: "04",
-    //     title: "What is the meaning of find out?",
-    //     alternatives: [
-    //         { alternative: "discover a fact or piece of information", isCorrect: true },
-    //         { alternative: "means to eventually decide something", isCorrect: false },
-    //         { alternative: "to have a good relationship with someone" , isCorrect: false},
+    //         { alternative: "To delay doing something", isCorrect: false },
+    //         { alternative: "To be careful of danger", isCorrect: true },
+    //         { alternative: "To tolerate something", isCorrect: false },
     //     ]
     // },
 ]
